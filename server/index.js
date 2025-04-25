e4b00076b22c432bd7507cee8841849680b24e5f
Bimal2002 <bimalgayali@gmail.com>
2025-04-25 15:03:55 +0530

    files

--- server/index.js
+++ server/index.js
@@ -14,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
 // Database connection
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/helpdesk', {
     useNewUrlParser: true,
